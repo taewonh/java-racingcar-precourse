ec1234f525c110a715a990190373e47e8d5ec0e6
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-07 17:02:24 +0900

    feat: 자동차 생성 기능 추가
    - 자동차 이름 길이 예외 처리 추가

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -2,7 +2,22 @@ package racingcar;
 
 public class Car {
 
+    private final String name;
     private int progress = 0;
+
+    public Car(String name) {
+        checkName(name);
+        this.name = name;
+    }
+
+    private static void checkName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("자동차 이름을 지정해야 합니다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름의 길이는 5글자 이하여야 합니다.");
+        }
+    }
 
     void increaseProgress(int num) {
         if (num >= 4) {
@@ -12,5 +27,9 @@ public class Car {
 
     public int getProgress() {
         return progress;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -6,15 +6,38 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
 
     @Test
+    public void 자동차_생성() {
+        String name = "tw";
+        Car car = new Car(name);
+        Assertions.assertEquals(name, car.getName());
+    }
+
+    @Test
+    public void 공백_이름_자동차_생성() {
+        String name = null;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Car car = new Car(name);
+        });
+    }
+
+    @Test
+    public void 글자수_초과_이름_자동차_생성() {
+        String name = "123456";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Car car = new Car(name);
+        });
+    }
+
+    @Test
     public void 자동차_전진_테스트() {
-        Car car = new Car();
+        Car car = new Car("name");
         car.increaseProgress(4);
         Assertions.assertEquals(1, car.getProgress());
     }
 
     @Test
     public void 자동차_멈춤_테스트() {
-        Car car = new Car();
+        Car car = new Car("name");
         car.increaseProgress(3);
         Assertions.assertEquals(0, car.getProgress());
     }
