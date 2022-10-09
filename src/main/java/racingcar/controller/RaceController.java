5f1505ac83a7c172574e6d1c70f3c90f15890d77
taewonh <40269387+taewonh@users.noreply.github.com>
2022-10-09 14:03:11 +0900

    refactor: 경주할 자동차 입력 요청 문구 변경
    - 프로그램 실행 결과 예시와 동일한 문구로 변경

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -13,7 +13,7 @@ public class RaceController {
     }
 
     public void readyToRace() {
-        System.out.println("경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println("경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         while (race.notRegisteredCars()) {
             inputRegisterCarNames();
         }
